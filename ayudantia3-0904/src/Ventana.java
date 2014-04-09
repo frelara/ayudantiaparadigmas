b9491cd14cb466f248c23520b0520679058c9381
Alumno <Alumno@eq35-lab1>
2014-04-09 19:58:41 -0300

    codigo del final de ayudantia

--- ayudantia3-0904/src/Ventana.java
+++ ayudantia3-0904/src/Ventana.java
@@ -1,8 +1,33 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 
 public class Ventana {
+public static void main(String[] args) {
 
-	public Ventana() {
-		// TODO Auto-generated constructor stub
+		JFrame frame = new JFrame();
+		JLabel label = new JLabel();
+		label.setText("Hola soy una Label");
+		frame.add(label);
+		JButton button = new JButton();
+		button.setText("Presioname!");
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "PRESIONADO");
+			}});
+		frame.add(button);
+		frame.setTitle("Mi segunda ventana");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+
+
 	}
-
 }
